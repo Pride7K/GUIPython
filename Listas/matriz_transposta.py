c994e259cc825e17845b662523980ed7c990e1e5
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-12-08 12:41:56 -0300

    arrumado codigo da matriz transposta
    antigo só funcionava para matriz quadradas

--- Listas/matriz_transposta.py
+++ Listas/matriz_transposta.py
@@ -6,11 +6,11 @@ nr_linhas = int(input("Digite quantas linhas tera a sua matriz: "));
 
 nr_colunas =int(input("Digite quantas colunas tera a sua matriz: "));
 
-matriz_transposta = []
 
-for i in range(0,nr_linhas):
+
+for i in range(nr_linhas):
     linha = []
-    for j in range(0,nr_colunas):
+    for j in range(nr_colunas):
         valor = float(input(f'Digite o valor da linha {i} e coluna {j}: '));
         print("");
         linha.append(valor);
@@ -20,19 +20,15 @@ for i in range(0,nr_linhas):
 
 
 
+
+
 matriz_transposta = []
 
-linha_tranposta = len(matriz);
-
-coluna_transposta = len(matriz[0]);
-
-print(matriz);
-
-for i in  range(nr_linhas):
+for i in  range(nr_colunas):
     coluna_t = []
-    for j in   range (nr_colunas):
+    for j in   range (nr_linhas):
         coluna_t.append(matriz[j][i]);
     print(coluna_t);
     matriz_transposta.append(coluna_t);
 
-print(matriz_transposta);
+
